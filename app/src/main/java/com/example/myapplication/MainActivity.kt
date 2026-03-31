15a2058eecefadb5da47719855dd58dc0513653d
Arsik <sosyanarsen@gmail.com>
2026-03-31 16:14:50 +0500

    6 задание

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,11 +20,12 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val editText = findViewById<EditText>(R.id.editText)
         val button = findViewById<Button>(R.id.button)
-        
+
         button.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
+            val text = editText.text.toString()
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
         }
     }
 }
